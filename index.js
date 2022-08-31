3d3af124b64d141f4895961a3b13659777350400
Chris Mann <chrispmann@gmail.com>
2022-08-31 10:45:39 -0700

    commit tree b077668143f2cfaaf89b39602a2bb2fd3af73588

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+ if (feet <= 400) {
+  return 'This one is on me!'
+ }
+ else if (feet <= 2000) {
+  return 'That will be twenty bucks.'
+  }
+else if (feet <= 2500){
+  return 'I will gladly take your thirty bucks.'
+}
+else if (feet >= 2501){
+  return 'No can do.'
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC" ? 'Ok, sounds good.': 'No go.');
+    
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous": return 'Thank you so much.';
+    case 'not as generous': return 'Thank you.';
+    default : return 'Bye.';
+  }
 }
+
+
+console.log(scuberGreetingForFeet(5000))
